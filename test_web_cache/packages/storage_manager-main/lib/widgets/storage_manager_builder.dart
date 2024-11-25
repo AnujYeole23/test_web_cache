a0ac8c7333818969ce6445dc5d388b364fec6543
anujyeole23 <anuj.yeole@centralogic.net>
2024-11-25 10:35:58 +0530

    updates : storage_manager_builder

--- test_web_cache/packages/storage_manager-main/lib/widgets/storage_manager_builder.dart
+++ test_web_cache/packages/storage_manager-main/lib/widgets/storage_manager_builder.dart
@@ -1,12 +1,13 @@
-import 'dart:io';
+import 'dart:io' if (dart.library.html) 'dart:html' as html;
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:storage_manager/controllers/storage_file_controller.dart';
 import 'package:storage_manager/enums/storage_file_status.dart';
 import 'package:storage_manager/models/storage_file_snapshot.dart';
+import 'package:http/http.dart' as http; // For fetching files on the web
 
-/// Using this widget it will download the file if not downloaded yet,
-/// if downloaded it will get it back in snapshot.
+/// A widget for downloading and managing storage files across platforms.
 class StorageFileBuilder extends StatefulWidget {
   const StorageFileBuilder({
     required this.storagePath,
@@ -16,16 +17,14 @@ class StorageFileBuilder extends StatefulWidget {
     super.key,
   });
 
-  /// URL of any type of media (Audio, Video, Image, etc...)
+  /// URL of any type of media (Audio, Video, Image, etc.)
   final String storagePath;
 
   final Directory? cacheDirectory;
 
   final DateTime? updateDate;
 
-  /// Snapshot Will provide you the status of process
-  /// (Success, Error, Loading)
-  /// and file if downloaded and download progress
+  /// Provides the current file download status and snapshot to build the UI.
   final Widget? Function(BuildContext context, StorageFileSnapshot snapshot)
       builder;
 
@@ -57,13 +56,60 @@ class _StorageFileBuilderState extends State<StorageFileBuilder> {
       },
     );
 
-    _storageFileController.getFile(
-      widget.storagePath,
-      cacheDir: widget.cacheDirectory,
-      updateDate: widget.updateDate,
-    );
-
+    _initFileDownload();
     super.initState();
+  }
+
+  Future<void> _initFileDownload() async {
+    if (Platform.isAndroid || Platform.isIOS || Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+      _storageFileController.getFile(
+        widget.storagePath,
+        cacheDir: widget.cacheDirectory,
+        updateDate: widget.updateDate,
+      );
+    } else {
+      // Web implementation
+      await _downloadFileForWeb(widget.storagePath);
+    }
+  }
+
+  Future<void> _downloadFileForWeb(String url) async {
+    try {
+      // Fetch the file data
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final blob = html.Blob([response.bodyBytes]);
+        final fileUrl = html.Url.createObjectUrlFromBlob(blob);
+
+        // Simulating a "downloaded" file path for the web
+        final simulatedPath = fileUrl;
+
+        // Update the snapshot
+        setState(() {
+          snapshot = StorageFileSnapshot(
+            status: StorageFileStatus.success,
+            filePath: simulatedPath,
+            progress: null,
+          );
+        });
+      } else {
+        setState(() {
+          snapshot = StorageFileSnapshot(
+            status: StorageFileStatus.error,
+            filePath: null,
+            progress: null,
+          );
+        });
+      }
+    } catch (e) {
+      setState(() {
+        snapshot = StorageFileSnapshot(
+          status: StorageFileStatus.error,
+          filePath: null,
+          progress: null,
+        );
+      });
+    }
   }
 
   @override
